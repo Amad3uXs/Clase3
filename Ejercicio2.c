4689c40733bf013a5dac255d73cb8377e4f6e276
Amad3uXs <davidmartveg@gmail.com>
2025-10-24 14:06:51 -0600

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -4,10 +4,12 @@ clase 3 ejercicio 2*/
 
 #include <stdio.h>
 
-void main() {
+void main() 
+{
     int i;
     
-    for (i = 0; i <= 10; i = i + 2) {
+    for (i = 0; i <= 10; i = i + 2) 
+    {
         printf("Num: %d\n", i);
     }
 }
