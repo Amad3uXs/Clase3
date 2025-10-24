b8dab07f107d63f975851a4206dd0b7536c45a69
Amad3uXs <davidmartveg@gmail.com>
2025-10-24 14:06:24 -0600

    Create Ejercicio4.c

---
+++ Ejercicio4.c
@@ -0,0 +1,13 @@
+/*David Alejandro Martínez Vega L´Englisse
+clase 3 ejercicio 1*/
+
+#include <stdio.h>
+
+void main() 
+{
+    for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            printf("Num: %d\n", i);
+        }
+    }
+}
