992fd62255266b2d9c07f3e52a4a6783acecc9e7
Amad3uXs <davidmartveg@gmail.com>
2025-10-24 13:55:30 -0600

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,1 +1,11 @@
+/*David Alejandro Martínez Vega L´Englisse
+clase 3 ejercicio 1*/
 
+#include <stdio.h>
+
+void main() 
+{
+    for (int i = 1; i <= 10; i++) {
+        printf("Num: %d\n", i);
+    }
+}
