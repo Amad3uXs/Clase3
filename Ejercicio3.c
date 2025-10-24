d623ab9e948e3fd25449406ab611a197b2ebb093
Amad3uXs <davidmartveg@gmail.com>
2025-10-24 14:06:38 -0600

    Update Ejercicio3.c

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -3,8 +3,10 @@ clase 3 ejercicio 3*/
 
 #include <stdio.h>
 
-void main() {
-    for (int i = 100; i >= 1; i--) {
+void main() 
+{
+    for (int i = 100; i >= 1; i--) 
+    {
         printf("Num: %d\n", i);
     }
 }
